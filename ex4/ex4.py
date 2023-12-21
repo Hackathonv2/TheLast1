e6adb2ccab5624243a69e97bb4f6f55751cdc91c
Valiance <lucas.ludovic@epitech.eu>
2023-12-21 10:46:54 +0100

    [ADDING FILE] Ex4 completed

--- ex4/ex4.py
+++ ex4/ex4.py
@@ -1,5 +1,33 @@
 import sys
 
-lines = []
-for line in sys.stdin:
-	lines.append(line.rstrip('\n'))
+
+def get_lines(lines):
+	for line in sys.stdin:
+		lines = line.rstrip('\n')
+	return lines
+
+
+def main():
+	chifumi = []
+	chifumi = get_lines(chifumi)
+	new_chifumi = ()
+	while len(chifumi) > 1:
+		if chifumi[0] == 'P' and chifumi[1] == 'F':
+			new_chifumi = chifumi[1:]
+			chifumi = new_chifumi
+			continue
+		if chifumi[0] == 'F' and chifumi[1] == 'C':
+			new_chifumi = chifumi[1:]
+			chifumi = new_chifumi
+			continue
+		if chifumi[0] == 'C' and chifumi[1] == 'P':
+			new_chifumi = chifumi[1:]
+			chifumi = new_chifumi
+			continue
+		new_chifumi = chifumi[:1] + chifumi[2:]
+		chifumi = new_chifumi
+	print(chifumi)
+
+
+if __name__ == '__main__':
+	main()
